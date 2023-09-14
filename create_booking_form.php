be9dda7a85a7c9a62a56c51c00431f036af070f2
unknown <cwitstudent@cw-it-350-d04.vtc.hk>
2023-09-14 21:09:29 +0800

    add todo list

--- create_booking_form.php
+++ create_booking_form.php
@@ -40,7 +40,7 @@
 
             <div class="form-group">
                 <label for="hkid">HKID:</label>
-                <input type="text" class="form-control" id="hkid" name="hkid" required>
+                <input type="text" class="form-control" id="hkid" name="hkid"  required>
             </div>
 
             <div class="form-group">
@@ -132,6 +132,40 @@
 
             return true; // Allow form submission
         }
+
+        function IsHKID(str) {
+            var strValidChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            if (str.length < 8)
+            { 
+                return false;
+            }
+            str = str.toUpperCase();    
+            var hkidPat = /^([A-Z]{1,2})([0-9]{6})([A0-9])$/;
+            var matchArray = str.match(hkidPat);    
+            if (matchArray == null)
+            {
+                return false;
+            }
+            var charPart = matchArray[1];
+            var numPart = matchArray[2];
+            var checkDigit = matchArray[3];    
+            var checkSum = 0;
+            if (charPart.length == 2) {
+                checkSum += 9 * (10 + strValidChars.indexOf(charPart.charAt(0)));
+                checkSum += 8 * (10 + strValidChars.indexOf(charPart.charAt(1)));
+            } else {
+                checkSum += 9 * 36;
+                checkSum += 8 * (10 + strValidChars.indexOf(charPart));
+            }
+
+            for (var i = 0, j = 7; i < numPart.length; i++, j--)
+            {
+                checkSum += j * numPart.charAt(i);
+            }
+            var remaining = checkSum % 11;
+            var verify = remaining == 0 ? 0 : 11 - remaining;
+            return verify == checkDigit || (verify == 10 && checkDigit == 'A');
+            }
     </script>
     <script src="index.js"></script>
 
